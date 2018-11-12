a40511ffc094a138befd70e5eb3f0f4d4177bd61
naokiur <atomic.transfer.comp@gmail.com>
2018-11-12 22:26:32 +0900

    [feat]Add POST test sample.

--- src/main/kotlin/jp/ne/naokiur/api/Executor.kt
+++ src/main/kotlin/jp/ne/naokiur/api/Executor.kt
@@ -5,6 +5,8 @@ import io.ktor.features.*
 import io.ktor.gson.*
 import io.ktor.http.*
 import io.ktor.request.receive
+import io.ktor.request.receiveMultipart
+import io.ktor.request.receiveParameters
 import io.ktor.response.*
 import io.ktor.routing.*
 import java.text.*
@@ -14,10 +16,12 @@ import java.text.DateFormat
 import javax.print.attribute.standard.Compression
 
 fun Application.main() {
+    data class Parameter(val message: String)
+
     val controller = UserController()
-    install(DefaultHeaders)
-    install(Compression)
-    install(CallLogging)
+//    install(DefaultHeaders)
+//    install(Compression)
+//    install(CallLogging)
     install(ContentNegotiation) {
         gson {
             setDateFormat(DateFormat.LONG)
@@ -32,6 +36,16 @@ fun Application.main() {
         get("/show") {
 //                call.respondText(controller.show(), ContentType.Text.Html)
             call.respond(controller.show())
+        }
+        post("/create") {
+            val parameter = call.receive<String>()
+//            val res = call.receiveParameters()
+//            println(parameter)
+            println(parameter)
+//            println(res)
+            call.respondText("create!!")
+
+//            println(parameter.message)
         }
     }
 }
--- src/test/kotlin/jp/ne/naokiur/api/ExecutorTest.kt
+++ src/test/kotlin/jp/ne/naokiur/api/ExecutorTest.kt
@@ -2,11 +2,18 @@ package jp.ne.naokiur.api
 
 import com.google.gson.GsonBuilder
 import io.ktor.application.Application
-import io.ktor.http.HttpMethod
-import io.ktor.http.HttpStatusCode
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.GsonConverter
+import io.ktor.http.*
 import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.setBody
 import io.ktor.server.testing.withTestApplication
 import jp.ne.naokiur.user.domain.infra.UserRepository
+import jp.ne.naokiur.user.domain.models.users.FullName
+import jp.ne.naokiur.user.domain.models.users.User
+import jp.ne.naokiur.user.domain.models.users.UserId
+import jp.ne.naokiur.user.domain.models.users.UserName
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -30,6 +37,27 @@ class ExecutorTest {
 
             assertEquals(HttpStatusCode.OK, response.status())
             assertEquals(expectedContent, response.content)
+        }
+    }
+
+    @Test
+    fun testCreate() = withTestApplication(Application::main) {
+        val gson = GsonBuilder().setPrettyPrinting().create()
+
+        val expectendUser = User(UserId(3), UserName("test"), FullName("test", "hoge"))
+
+        handleRequest(HttpMethod.Post, "/create") {
+            println(ContentType.Application.Json.toString())
+            addHeader(HttpHeaders.Accept, ContentType.Text.Plain.toString())
+            addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            setBody("test")
+
+//            setBody(listOf("id" to "3", "userName" to "test", "firstName" to "test", "lastName" to "hoge").formUrlEncode())
+        }.run {
+            val expectedContent = gson.toJson(repository.findAll())
+
+            assertEquals(HttpStatusCode.OK, response.status())
+//            assertEquals(expectedContent, response.content)
         }
     }
 }
