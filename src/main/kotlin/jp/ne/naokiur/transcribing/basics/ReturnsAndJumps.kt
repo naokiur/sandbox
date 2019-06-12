cad3f1a4948869ea2a8ae700653a9a0a6de964aa
naokiur <atomic.transfer.comp@gmail.com>
2019-06-12 10:31:52 +0900

    Transcribing return and jump.

--- src/main/kotlin/jp/ne/naokiur/transcribing/basics/ReturnsAndJumps.kt
+++ src/main/kotlin/jp/ne/naokiur/transcribing/basics/ReturnsAndJumps.kt
@@ -1,10 +1,15 @@
 package jp.ne.naokiur.transcribing.basics
 
+import java.util.*
+import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
+
 class ReturnsAndJumps {
     fun execute() {
         println("${this.javaClass.name} begin.")
 
         largerExpressions()
+        breakAndContinueLabels()
+        returnAtLabel()
 
         println("${this.javaClass.name} end.")
     }
@@ -20,7 +25,78 @@ class ReturnsAndJumps {
 
     private fun breakAndContinueLabels() {
 
+        fun withOutLabel() {
+            println("withOutLabel")
+            for (i in 1..10) {
+                for (j in 1..100) {
+                    if (j == 2) {
+                        break
+                    }
+                    println("i: $i, j: $j")
+                }
+            }
+        }
+        withOutLabel()
+
+        fun withLabael() {
+            println("withLabel")
+
+            loop@ for (i in 1..10) {
+                for (j in 1..100) {
+                    if (j == 2) {
+                        break@loop
+                    }
+                    println("i: $i, j: $j")
+                }
+            }
+        }
+        withLabael()
     }
 
+    private fun returnAtLabel() {
+        fun withOutLabel() {
+            println("withOutLabel")
+            listOf(1, 2, 3, 4, 5).forEach {
+                if (it == 3 ) return
+                println(it)
+            }
+            println("this point is unreachable")
+        }
+        withOutLabel()
 
+        fun withLabel() {
+            println("withLabel")
+            listOf(1, 2, 3, 4, 5).forEach lit@{
+                if (it == 3 ) return@lit
+                println(it)
+            }
+
+            println("done with explicit label")
+        }
+        withLabel()
+
+        fun withAnonymousFunction() {
+            println("withAnonymousFunction")
+            listOf(1, 2, 3, 4, 5).forEach(fun (value: Int){
+                if (value == 3) return
+                println(value)
+            })
+
+            println("done with anonymous function")
+        }
+        withAnonymousFunction()
+
+        fun nestedLoop() {
+            println("nestedLoop")
+            run loop@{
+                listOf(1, 2, 3, 4, 5).forEach {
+                    if (it == 3) return@loop
+                    println(it)
+                }
+            }
+
+            println("done with nested loop")
+        }
+        nestedLoop()
+    }
 }
