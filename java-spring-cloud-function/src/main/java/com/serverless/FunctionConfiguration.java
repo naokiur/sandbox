9d25eb7d26e798f68120e345518e1ba658cdf3eb
naokiur <atomic.transfer.comp@gmail.com>
2024-06-18 16:05:05 +0900

    try(java/gradle): for Spring Cloud Function only.

---
+++ java-spring-cloud-function/src/main/java/com/serverless/FunctionConfiguration.java
@@ -0,0 +1,20 @@
+package com.serverless;
+
+import java.util.function.Function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FunctionConfiguration {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FunctionConfiguration.class, args);
+  }
+
+  @Bean
+  public Function<String, String> uppercase() {
+    return String::toUpperCase;
+  }
+}
