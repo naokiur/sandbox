f5ac5f6dba4804598673fc3e602391c5bea91bd8
naokiur <atomic.transfer.comp@gmail.com>
2024-06-25 15:46:15 +0900

    try(java/gradle): replace by Spring boot. I did it.

---
+++ java-spring-cloud-function/src/main/java/com/naokiur/serverless/sample/Application.java
@@ -0,0 +1,20 @@
+package com.naokiur.serverless.sample;
+
+import java.util.function.Function;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+	@Bean
+	public Function<String, String> uppercase() {
+		return value -> value.toUpperCase();
+	}
+}
